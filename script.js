6ebd42e410fef25d0890d5d30544dffc9bc20af6
damaresgaia <damaresgaia@gmail.com>
2024-10-30 08:08:03 -0300

    Lista de Tarefas

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function validarFormulario(event) {
-    event.preventDefault(); // Previne o envio do formulário
-
-    // Obtém os valores dos campos A e B
-    const campoA = parseFloat(document.getElementById("campoA").value);
-    const campoB = parseFloat(document.getElementById("campoB").value);
-
-    // Verifica se o número B é maior que o número A
-    if (campoB > campoA) {
-        alert("Formulário válido! O número B é maior que o número A.");
-    } else {
-        alert("Formulário inválido. O número B deve ser maior que o número A.");
-    }
-}
+$(document).ready(function () {
+    $('#formularioTarefa').submit(function (evento) {
+      evento.preventDefault();
+  
+      const textoTarefa = $('#entradaTarefa').val();
+      if (textoTarefa) {
+        $('#listaTarefas').append(`<li>${textoTarefa}</li>`);
+        $('#entradaTarefa').val('');
+      }
+    });
+  
+    $('#listaTarefas').on('click', 'li', function () {
+      $(this).toggleClass('completada');
+    });
+  });
+  
